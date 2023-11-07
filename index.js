232f820ea7befb3f1b164317b296ccafb97388e4
opilane <infoman724@gmail.com>
2023-11-07 14:55:59 +0200

    [delivery#186423849] As an app developer I want to get a full details of a given games to show in my app

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const app = require('express')()
 const port = 8080
 const swaggerUi = require('swagger-ui-express')
-const swaggerDoucemnt = require('./docs/swagger.json');
+const yamljs = require('yamljs')
+const swaggerDoucemnt = yamljs.load('./docs/swagger.yaml');
 
-app.use('/docs',swaggerUi.serve,swaggerUi.setup(swaggerDoucemnt))
+
+
+const games = [
+    {id: 1,name:"Baldurs gate 3",price: 10.99},
+    {id: 2,name:"Dota 2",price: 20.99},
+    {id: 3,name:"Dayz",price: 30.99}
+]
 
 app.get('/games',(req,res)=>{
-    res.send(["Baldurs gate 3","Dota 2","Dayz"])
+    res.send(games)
 })
+app.get('/games/:id',(req,res)=>{
+    if(typeof games[req.params.id - 1]==='undefined'){
+        return res.status(404).send({error: "Game not found"})
+    }
+
+    res.send(games[req.params.id -1])
+})
+
 
 app.listen(port,()=>{
     console.log(`Api up at: http://localhost:${port}`)
 })
+
+app.use('/docs',swaggerUi.serve,swaggerUi.setup(swaggerDoucemnt))
